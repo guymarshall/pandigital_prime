55b43990912d9a0ff209f6d6cae1ac626d2438b9
Guy Marshall <guymarshall9@gmail.com>
2022-10-28 18:04:23 +0100

    Add is_pandigital

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,34 @@ What is the largest n-digit pandigital prime that exists?
 
 use rayon::prelude::*;
 
-fn is_prime(number: &i32) -> bool {
+fn is_prime(number: &i64) -> bool {
     !(2..*number).any(|n| number % n == 0)
 }
 
+fn is_pandigital(number: i64) -> bool {
+    let digits: String = number.to_string();
 
+    let one_count: usize = digits.matches("1").count();
+    let two_count: usize = digits.matches("2").count();
+    let three_count: usize = digits.matches("3").count();
+    let four_count: usize = digits.matches("4").count();
+    let five_count: usize = digits.matches("5").count();
+    let six_count: usize = digits.matches("6").count();
+    let seven_count: usize = digits.matches("7").count();
+    let eight_count: usize = digits.matches("8").count();
+    let nine_count: usize = digits.matches("9").count();
+
+    one_count == 1 && two_count == 1 && three_count == 1 && four_count == 1 && five_count == 1 && six_count == 1 && seven_count == 1 && eight_count == 1 && nine_count == 1
+}
 
 fn main() {
-    let primes: Vec<i32> = (123456789..=123457899).into_par_iter().filter(is_prime).collect::<Vec<i32>>();
+    let primes: Vec<i64> = (123456789..=123457899).into_par_iter().filter(is_prime).collect::<Vec<i64>>();
 
-    println!("Primes: {:#?}", primes);
+    // let mut numbers: Vec<i64> = vec![1, 2, 123456789, 987654321, 983217465];
+
+    for number in primes {
+        println!("{} is pandigital: {}", number, is_pandigital(number));
+    }
+
+    // println!("Primes: {:#?}", primes);
 }
