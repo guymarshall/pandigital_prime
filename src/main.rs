871bfab6082c7de66fa739a3d4088cc59e0a79e3
Guy Marshall <guymarshall9@gmail.com>
2022-10-28 17:47:22 +0100

    Remove pub from is_prime

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ What is the largest n-digit pandigital prime that exists?
 
 use rayon::prelude::*;
 
-pub fn is_prime(number: &i32) -> bool {
+fn is_prime(number: &i32) -> bool {
     !(2..*number).any(|n| number % n == 0)
 }
+
+
 
 fn main() {
     let primes: Vec<i32> = (123456789..=123457899).into_par_iter().filter(is_prime).collect::<Vec<i32>>();
