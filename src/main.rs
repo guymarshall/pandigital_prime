c64966dd150ed847d0c81adcb25340d2d454814a
Guy Marshall <guymarshall9@gmail.com>
2022-10-28 17:14:43 +0100

    Remove useless comment

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,6 @@ mod user_input;
 fn main() {
     let user_input: u128 = user_input::get_user_input("Enter a positive integer: ");
 
-    // maybe do logic so if number above a certain amount, move from u8 to u16, u32 etc
-
     let mut prime_number_count: u128 = 0;
     let mut number: u128 = 2;
 
